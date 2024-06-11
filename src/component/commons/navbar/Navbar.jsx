c037e26e5b60d62bb2481f706661eeb23f595d1c
Hanzlazahid <hanzlazahid488@gmail.com>
2024-06-11 19:15:49 +0500

    nav z-index

--- src/component/commons/navbar/Navbar.jsx
+++ src/component/commons/navbar/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
                 </div>
             </div>
 
-            <div className={`fixed top-[110px] left-1/2 transform -translate-x-1/2 w-4/5 py-3 border border-nav bg-white z-10 rounded-lg shadow-lg transition-all duration-300 ${isMobileNavOpen ? 'opacity-100 scale-100' : 'opacity-0 scale-90 pointer-events-none'}`}>
+            <div className={`fixed top-[110px] left-1/2 transform -translate-x-1/2 w-4/5 py-3 border border-nav bg-white z-20 rounded-lg shadow-lg transition-all duration-300 ${isMobileNavOpen ? 'opacity-100 scale-100' : 'opacity-0 scale-90 pointer-events-none'}`}>
                 <div className='flex flex-col justify-center items-center pb-2'>
                     {links.map((link, index) => (
                         <Link key={index} href={link.link} passHref className='font-syne text-nav hover:underline hover:text-black duration-1000 underline-offset-8 py-2'>
